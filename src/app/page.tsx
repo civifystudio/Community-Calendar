f418b14fae6e675905d431a48563dfaf72c455e7
Rodriguez Christopher <thisismyfirst434@gmail.com>
2025-07-01 16:29:36 +0000

    make a blank page on the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1>This page is intentionally blank.</h1>
     </main>
   );
 }
