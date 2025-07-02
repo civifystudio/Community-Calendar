ac0757cf0501ac6919a11c50b7d25a86620b028a
civifystudio <civifystudio@gmail.com>
2025-07-02 19:17:21 +0000

    remove the outline of the top bar please.

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function AppHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="sticky top-0 z-40 w-full bg-background">
       <div className="container flex h-16 items-center justify-center">
         <nav className="flex items-center gap-6 md:gap-10">
             <Link
